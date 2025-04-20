1247889f32e48865419b2ad9905e8a77afd09194
ChinmayRaj <chinmayshah2002@gmail.com>
2025-04-20 23:29:42 +0530

    commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css';
-import  profile_img from'../../assets/myhero2.png';
+import  profile_img from'../../assets/hero69.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 
